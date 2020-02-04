bed11ced010b0a6564ad73a53d698afee62774cd
cyberspace009 <cyberspace009@bitbucket.org>
2020-02-03 22:07:26 -0700

    First commit of project.

---
+++ ghostProject/Assets/Scripts/ControllerGrab.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ControllerGrab : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean grabAction;
+
+    private GameObject collidingObject; 
+    private GameObject objectInHand;
+
+    public bool GetGrab() // 2
+    {
+        return grabAction.GetState(handType);
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+        
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        
+        collidingObject = col.gameObject;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    private void GrabObject()
+    {
+        
+        objectInHand = collidingObject;
+        collidingObject = null;
+        
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    private void ReleaseObject()
+    {
+        
+        if (GetComponent<FixedJoint>())
+        {
+            
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            
+            objectInHand.GetComponent<Rigidbody>().velocity = controllerPose.GetVelocity();
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = controllerPose.GetAngularVelocity();
+
+        }
+        
+        objectInHand = null;
+    }
+
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetGrab())
+        {
+            print("Grab " + handType);
+        }
+
+        if (grabAction.GetLastStateDown(handType))
+        {
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+        }
+
+        if (grabAction.GetLastStateUp(handType))
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+}
---
+++ ghostProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerController : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType; 
+    //public SteamVR_Action_Boolean teleportAction; 
+    //public SteamVR_Action_Boolean grabAction;
+    public SteamVR_Action_Boolean move;
+    public SteamVR_Action_Boolean rotateRight;
+    public SteamVR_Action_Boolean rotateLeft;
+
+    public GameObject player;
+    public GameObject playerCamera;
+    private float _playerSpeed;
+    private Vector3 _playerPosition;
+    private float _playerRotation;
+    private float _playerRotationSpeed;
+
+    void Start()
+    {
+        move.AddOnStateDownListener(DpadNorthDown, handType);
+        move.AddOnStateUpListener(DpadNorthUp, handType);
+
+        rotateRight.AddOnStateDownListener(DpadEastDown, handType);
+        rotateRight.AddOnStateDownListener(DpadEastUp, handType);
+
+        rotateLeft.AddOnStateDownListener(DpadWestDown, handType);
+        rotateLeft.AddOnStateDownListener(DpadWestUp, handType);
+    }
+
+    public void DpadNorthUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("dpad north is up");
+        _playerSpeed = 0.0f;
+        _playerRotationSpeed = 0.0f;
+    }
+
+    public void DpadNorthDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("dpad north is down");
+        Movement();
+    }
+
+    public void DpadEastUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        _playerRotationSpeed = 0.0f;
+    }
+
+    public void DpadEastDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        RotateEast();
+    }
+
+    public void DpadWestUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        _playerRotationSpeed = 0.0f;
+    }
+
+    public void DpadWestDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        RotateWest();
+    }
+
+    public void Movement() {
+        _playerSpeed = 2.0f;
+        _playerRotationSpeed = 1.0f;
+        _playerPosition = new Vector3(0.0f, 0.0f, 1.0f);
+    }
+
+    public void RotateEast()
+    {
+        _playerRotationSpeed = 1.0f;
+        player.GetComponent<Rigidbody>().transform.Rotate(0.0f, 45 * _playerRotationSpeed, 0.0f);
+    }
+
+    public void RotateWest()
+    {
+        _playerRotationSpeed = 1.0f;
+        player.GetComponent<Rigidbody>().transform.Rotate(0.0f, -45 * _playerRotationSpeed, 0.0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //if (GetTeleportDown())
+        //{
+        //    print("Teleport " + handType);
+        //}
+
+        //if (GetGrab())
+        //{
+        //    print("Grab " + handType);
+        //}
+
+        _playerRotation = playerCamera.transform.localRotation.y;
+        player.GetComponent<Rigidbody>().transform.Rotate(0.0f, _playerRotation * _playerRotationSpeed, 0.0f);
+        player.GetComponent<Rigidbody>().transform.Translate(_playerPosition * _playerSpeed * Time.deltaTime);
+    }
+}
